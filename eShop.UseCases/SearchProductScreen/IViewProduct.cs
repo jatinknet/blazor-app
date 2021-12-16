ac32223709bf7050bc1f0de0b9bcbdd5d41363be
Jatin Kumar <jatink.net@gmail.com>
2021-12-16 13:40:01 +0530

    Implemented Core business logic and use cases

---
+++ eShop.UseCases/SearchProductScreen/IViewProduct.cs
@@ -0,0 +1,14 @@
+﻿using eShop.CoreBusiness.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.SearchProductScreen
+{
+   public interface IViewProduct
+    {
+        public Product Execute(int id);
+    }
+}
